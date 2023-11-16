d8636983bad2a4982e389aae52f66b4e73948124
Daniel Souza <workdndsza@gmail.com>
2023-11-16 13:47:23 -0300

    criando as properties

--- src/main/java/com/dnsouzadev/todolist/entity/Todo.java
+++ src/main/java/com/dnsouzadev/todolist/entity/Todo.java
@@ -1,6 +1,5 @@
 package com.dnsouzadev.todolist.entity;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
